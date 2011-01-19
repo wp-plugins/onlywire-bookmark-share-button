f7886537ab71a3f5adc90029fdcae9a83fa56c81
craiglurey <craiglurey@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-19 11:08:04 +0000

    git-svn-id: https://plugins.svn.wordpress.org/onlywire-bookmark-share-button/tags/1.6.3@334541 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/iframe.php
+++
@@ -1,8 +0,0 @@
-<?php
-include ("postrequest.php");
-//$site = file_get_contents('http://onlywire.com/thebuttonwp');
-//echo $site;
-
-$site = GetRequest('http://onlywire.com/thebuttonwp');
-echo $site[1];
-?>
