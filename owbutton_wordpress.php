ea5804aaa8a798892c662073253c6bfbb01b1430
craiglurey <craiglurey@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-23 09:45:38 +0000

    update encodeURIToComponent
    git-svn-id: https://plugins.svn.wordpress.org/onlywire-bookmark-share-button/trunk@479718 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- owbutton_wordpress.php
+++ owbutton_wordpress.php
@@ -226,7 +226,7 @@ function verifyAutoRevisions() {
 function auth() {
 	 var ow_username = document.getElementById("ow_username").value;
 	 var ow_password = document.getElementById("ow_password").value;
-	 var url = "<?php echo site_url()?>/wp-content/plugins/onlywire-bookmark-share-button/http_auth_call.php?auth_user="+ow_username+"&auth_pw="+ow_password;
+	 var url = "<?php echo site_url()?>/wp-content/plugins/onlywire-bookmark-share-button/http_auth_call.php?auth_user="+encodeURIComponent(ow_username.trim())+"&auth_pw="+encodeURIComponent(ow_password.trim());
 	 var xmlhttp;
 	
 	if (window.XMLHttpRequest)
