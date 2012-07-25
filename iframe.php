618e119dbb2ff9e06f562323d15820ba0f2ceb22
craiglurey <craiglurey@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-25 17:27:04 +0000

    Changed to work for 3.4.1
    git-svn-id: https://plugins.svn.wordpress.org/onlywire-bookmark-share-button/trunk@577275 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- iframe.php
+++ iframe.php
@@ -3,6 +3,6 @@ include ("postrequest.php");
 //$site = file_get_contents('http://onlywire.com/thebuttonwp');
 //echo $site;
 
-$site = GetRequest('http://onlywire.com/thebuttonwp');
+$site = GetRequest('http://www.onlywire.com/thebuttonwp');
 echo $site[1];
 ?>
