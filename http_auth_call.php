29511f46080401da4b4a045edfaf18d392dabcd2
craiglurey <craiglurey@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-25 17:26:03 +0000

    Changed to work for 3.4.1
    git-svn-id: https://plugins.svn.wordpress.org/onlywire-bookmark-share-button/trunk@577274 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- http_auth_call.php
+++ http_auth_call.php
@@ -3,7 +3,7 @@ include ("postrequest.php");
 // this file takes care of posting the response data to onlywire to confirm user's authetication
 extract($_GET);
 
-$url = "http://onlywire.com/widget/http_auth.php?auth_user=".urlencode($auth_user)."&auth_pw=".urlencode($auth_pw);
+$url = "http://www.onlywire.com/widget/http_auth.php?auth_user=".urlencode($auth_user)."&auth_pw=".urlencode($auth_pw);
 $a = EncodePostRequest($url,"", $_GET);
 
 echo $a[1];
