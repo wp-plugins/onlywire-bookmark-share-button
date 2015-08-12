a448ac5453f9c149730321f5e0c6e59c305a5f6c
craiglurey <craiglurey@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-12 18:04:04 +0000

    update to 1.9.4
    git-svn-id: https://plugins.svn.wordpress.org/onlywire-bookmark-share-button/tags/1.9.4@1219410 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ http_auth_call.php
@@ -0,0 +1,9 @@
+<?php
+include ("owConfig.php");
+include ("postrequest.php");
+// this file takes care of posting the response data to onlywire to confirm user's authetication
+extract($_GET);
+
+$data = checkUser($auth_user, $auth_pw);
+echo $data;
+?>
